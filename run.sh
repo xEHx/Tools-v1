ea55ae3f31f0fa764890d8cdb8093d0599be1bf2
xEHx <55247913+xEHx@users.noreply.github.com>
2019-10-03 20:38:50 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,221 @@
+#!/system/bin/sh
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+clear
+echo""
+echo""
+echo $me "                    ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶                    " 
+sleep 0.03
+echo $me "                ¶¶¶¶¶¶             ¶¶¶¶¶¶¶               " 
+sleep 0.03
+echo $me "             ¶¶¶¶                        ¶¶¶¶            " 
+sleep 0.03
+echo $me "            ¶¶¶                            ¶¶¶           " 
+sleep 0.03
+echo $me "           ¶¶                               ¶¶           " 
+sleep 0.03
+echo $me "          ¶¶                                 ¶¶          " 
+sleep 0.03
+echo $me "          ¶¶                                 ¶¶          " 
+sleep 0.03
+echo $me "          ¶¶´¶¶                           ¶¶´¶¶          " 
+sleep 0.03
+echo $me "          ¶¶´¶¶                           ¶¶´¶¶          " 
+sleep 0.03
+echo $me "          ¶¶´¶¶                           ¶¶´¶¶          " 
+sleep 0.03
+echo $me "         ¶¶ ¶¶                             ¶¶ ¶¶         " 
+sleep 0.03
+echo $me "          ¶¶´´¶¶                         ¶¶´´¶¶          " 
+sleep 0.03
+echo $me "           ¶¶´¶¶   ¶¶¶¶¶¶¶¶   ¶¶¶¶¶¶¶¶   ¶¶´¶¶           " 
+sleep 0.03
+echo $me "            ¶¶¶¶´¶¶¶¶¶¶¶¶¶¶   ¶¶¶¶¶¶¶¶¶¶'¶¶¶¶            " 
+sleep 0.03
+echo $me "             ¶¶¶´¶¶¶¶¶¶¶¶¶¶   ¶¶¶¶¶¶¶¶¶¶'¶¶¶             " 
+sleep 0.03
+echo $me "    ¶¶¶      ¶¶  ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶    " 
+sleep 0.03
+echo $me "   ¶¶¶¶¶    ¶¶    ¶¶¶¶¶¶¶  ¶¶¶  ¶¶¶¶¶¶¶     ¶¶   ¶¶¶¶¶   " 
+sleep 0.03
+echo $me "  ¶¶   ¶¶   ¶¶      ¶¶¶   ¶¶¶¶¶   ¶¶¶      ¶¶   ¶¶   ¶¶  " 
+sleep 0.03
+echo $me " ¶¶¶    ¶¶¶¶ ¶¶          ¶¶¶¶¶¶¶          ¶¶ ¶¶¶¶    ¶¶¶ " 
+sleep 0.03
+echo $me "¶¶        ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶        ¶¶" 
+sleep 0.03
+echo $me "¶¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶¶" 
+sleep 0.03
+echo $me "  ¶¶¶¶ ¶¶¶¶¶      ¶¶¶¶¶           ¶¶¶¶¶      ¶¶¶¶¶ ¶¶¶¶  " 
+sleep 0.03
+echo $me "       ¶¶¶¶¶¶  ¶¶¶  ¶¶             ¶¶  ¶¶¶  ¶¶¶¶¶¶       "
+sleep 0.03
+echo $me "              ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶              " 
+sleep 0.03
+echo $me "                  ¶¶ ¶¶ ¶ ¶ ¶ ¶ ¶ ¶¶ ¶¶                  " 
+sleep 0.03
+echo $me "                ¶¶¶¶ ¶¶ ¶ ¶ ¶ ¶ ¶ ¶¶ ¶¶¶¶                " 
+sleep 0.03
+echo $me "            ¶¶¶¶¶ ¶¶  ¶¶¶¶¶¶¶¶¶¶¶¶¶  ¶¶ ¶¶¶¶¶            " 
+sleep 0.03
+echo $me "    ¶¶¶¶¶¶¶¶¶¶     ¶¶               ¶¶     ¶¶¶¶¶¶¶¶¶     " 
+sleep 0.03
+echo $me "   ¶¶           ¶¶¶¶¶¶¶           ¶¶¶¶¶¶¶          ¶¶    " 
+sleep 0.03
+echo $me "    ¶¶¶     ¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶    ¶¶¶     " 
+sleep 0.03
+echo $me "      ¶¶   ¶¶¶          ¶¶¶¶¶¶¶¶¶          ¶¶¶  ¶¶       " 
+sleep 0.03
+echo $me "      ¶¶  ¶¶                                ¶¶  ¶¶       " 
+sleep 0.03
+echo $me "       ¶¶¶¶                                  ¶¶¶¶        "
+sleep 0.03
+echo $me ""
+echo $me ""
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo $i "CREATOR  : xEHx"
+echo $i "TOOLS    : INSTALLER"
+echo $i "GMAIL    : xEHx.com@gmail.com"
+echo $i "TEAM     : HackCat"
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo $cy " Hello,let me know,what is your name??"
+echo $cy ">>>>"
+read $nick
+figlet $nick
+sleep 2
+echo $i Hello $nick "!"
+sleep 1
+echo "Loading....."
+sleep 2
+echo "Open Script...."
+sleep 
+clear
+figlet $ick
+echo $cy " ___TOOL LIST___"
+echo $me "[1]"$me" FACEBOOK HACK"
+echo $me "[2]"$me" Spam Chat"
+echo $me "[3]"$me" Phising Web Maker"
+echo $me "[4]"$me" CCTV Hack"
+echo $me "[5]"$me" DDOS Attack"
+echo $me "[6]"$me" Virus Maker"
+echo $me "[7]"$me" Camera Hack"
+echo $me "[8]"$me" Web Deface"
+echo $me "[9]"$me" Join HackCat"
+echo $me "[10]"$me" EXIT"
+echo 
+echo 
+echo $cy"┌==="$bi"[ xEHx ]"
+echo $cy"¦"
+read -p">>>" pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/storiku/darkfb
+cd darkfb
+python2 Dark.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+bash troll.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/Zen-Clay/zenphis
+cd zenphis
+sh zenphis.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+pkg install python
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+echo $me "AFTER DONE,U NEED TO DO cd Android"
+echo $me "AND MOVE IT TO SDCARD USING mv (VIRUS NAME) /sdcard"
+echo $me "xEHx Here.."
+sleep 13
+pkg install python2
+git clone https://github.com/Mr-xDODOL/Malicious
+cd Malicious
+unzip Malicious
+cd Malicious
+pip2 install -r requirements.txt
+pip2 install tqdm
+chmod 777 malicious.py
+python2 malicious.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+chmod 777 webdav77.sh
+bash install.sh
+fi
+
+if [ $pil = 9 ]
+then
+clear
+echo $me "LOADING...."
+sleep 3
+echo $me "HERE,COPY THIS!..."
+echo $me "===>>> H4ckCat.000webhostapp.com"
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Thanks for use my script"
+sleep 2
+echo $i"If have some issue,DM me on Gmail only"
+sleep 2
+echo $i" GMAIL :"$i" xEHx.com@gmail.com"
+echo $bi"CREATOR :"$i" xEHx"
+exit
+fi
+
+
