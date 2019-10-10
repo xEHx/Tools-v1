ccfb7f89c26575b9a9813402ceb14c623354b7d3
xEHx <55247913+xEHx@users.noreply.github.com>
2019-10-10 18:26:14 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -92,10 +92,9 @@ echo $i "GMAIL    : xEHx.com@gmail.com"
 echo $i "TEAM     : HackCat"
 echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo $cy "Hello,let me know,what is your name??"
-echo $me "---[Your Name]"
-echo $me "|"
-echo $me "|"
-read -p"-->>>" nick;
+echo $me "┌===[Your Name]"
+echo $me "¦"
+read -p">>>" nick;
 figlet $nick
 sleep 0.03
 echo Hello $nick "!"
